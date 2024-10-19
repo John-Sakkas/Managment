4f48bfc28529698f3e5cd8739df228221c420aff
Giannis Sakkas <troumjohn@gmail.com>
2024-10-19 15:27:11 +0300

    Refactor

--- StaticVariables.cs
+++ StaticVariables.cs
@@ -10,9 +10,9 @@ namespace Managment
     {
         public static string connectionString = "Data Source=John_S-PC;Initial Catalog=MANAGMENTDB;Integrated Security=True;Trust Server Certificate=True";
 
+        //DB Table Names
         public static string baseDB = "BASEDB";
         public static string mattressDB = "MATTRESSDB";
         public static string fabricDB = "FABRICDB";
-
     }
 }
